69e3543f20f971aba4de3747491c4a1ce9d7c510
jeleo stephen enrile <jeleostephenenrile731@gmail.com>
2024-01-25 16:12:24 +0800

    Added Entity Framework and Created Database

---
+++ HotelListingAPI/Data/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Address { get; set; }
+
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+    }
+
+  
+}
