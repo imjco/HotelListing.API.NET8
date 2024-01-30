4bd32406ada33221d74d582368aeb2549aa39582
jeleo stephen enrile <jeleostephenenrile731@gmail.com>
2024-01-30 14:09:02 +0800

    trigger

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -42,7 +42,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
+    app.UseSwagger(); 
+
     app.UseSwaggerUI();
 //}
 
